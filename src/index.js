4de0ccef28dc89aba5a0cd4059a3eb6e8c3bcfca
Federico Andrucci <federico.andrucci@gmail.com>
2022-02-16 17:36:06 +0100

    organizzazione directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('../config.json');
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
